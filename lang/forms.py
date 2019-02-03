4ca42b1807fc046e800e81b7ca7c72eea525f1f8
PrOrOk26 <vraniuk@gmail.com>
2019-02-04 00:29:07 +0200

    Models v.0.1

--- lang/forms.py
+++ lang/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+from .models import LangUser
 from django.core.exceptions import ObjectDoesNotExist
 from django.forms import ModelForm
+from django.core.validators import MinLengthValidator
 
-class SignInForm(forms.Form):
-    username = forms.CharField(label="Login", min_length=5)
-    password = forms.CharField(widget=forms.PasswordInput(render_value=False), label="Password", min_length=5)
+class SignInForm(forms.ModelForm):
+
+    class Meta:
+        model = LangUser
+        fields = ("username", "password", )
+
+    def __init__(self, *args, **kwargs):
+        super(SignInForm, self).__init__(*args, **kwargs)
+        self.fields['password'].widget = forms.PasswordInput(
+                render_value=False,
+                )
+        self.fields['username'].validators.append(MinLengthValidator(limit_value=5))
+        self.fields['password'].validators.append(MinLengthValidator(limit_value=5))
 
     def log_in(self, request):
         username = self.cleaned_data.get('username')
@@ -18,22 +29,30 @@ class SignUpForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput(render_value=False), label="confirmPassword", min_length=5)
     
     class Meta:
-        model = User
+        model = LangUser
         fields = ("username", "password", "email", )
-        widgets = {
-            "password": forms.PasswordInput(render_value=False),
-        }
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.fields['password'].widget = forms.PasswordInput(
+                render_value=False,
+                )
+        self.fields['username'].validators.append(MinLengthValidator(limit_value=5))
+        self.fields['password'].validators.append(MinLengthValidator(limit_value=5))
+        self.fields['confirm_password'].validators.append(MinLengthValidator(limit_value=5))
+        
 
     def sign_up(self):
         try:
             username = self.cleaned_data.get('username')
             password = self.cleaned_data.get('password')
             email = self.cleaned_data.get('email')
-            user_check = User.objects.get(username=username)
+            user_check = LangUser.objects.get(username=username)
             return None
         except ObjectDoesNotExist:
-            user_check = User.objects.create_user(username=username,
-            password=password,
-            email=email)
+            user_check = LangUser.objects.create_user(username=username,
+            password=password, )
+            user_check.email = email
+            user_check.save()
             return user_check
         
--- lang/models.py
+++ lang/models.py
@@ -1,3 +1,78 @@
-from django.db import models
+from django.db.models import (
+    CharField, EmailField,
+    DateField, BooleanField,
+    Model, ManyToManyField,
+    ManyToManyField, ManyToOneRel, 
+)
 
-# Create your models here.
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, 
+)
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError("Username must be specified!")
+        user = self.model(
+            username=self.username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, password):
+        user = self.create_user(
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class Language(Model):
+    pass
+
+class LangUser(AbstractBaseUser):
+    username = CharField(
+        verbose_name='Username',
+        max_length=255,
+        primary_key=True,
+        unique=True,
+    )
+    first_name = CharField(verbose_name="First name",max_length=255,)
+    last_name = CharField(verbose_name="Last name",max_length=255,)
+    country = CharField(verbose_name="Country",max_length=255,)
+    email = EmailField(
+        verbose_name='Email address',
+        max_length=255,
+    )
+    date_of_birth = DateField(verbose_name="Date of birth")
+    languages = ManyToManyField(Language, through='UserLearnsLanguage')
+
+    is_active = BooleanField(default=True)
+    is_admin = BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username', 'password', ]
+
+    def __str__(self):
+        return self.username
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return f"{self.first_name}"
+
+    def save(self, *args, **kwargs):
+        super(LangUser, self).save(*args, **kwargs)
+
+class UserLearnsLanguage(Model):
+    pass
+
+class Word(Model):
+    pass
